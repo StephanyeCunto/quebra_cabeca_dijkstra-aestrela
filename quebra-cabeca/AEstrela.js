44ebc67ba966672420accdf98f879eccad973286
StephanyeCunto <stephanyecristine6@gmail.com>
2025-08-31 23:37:27 -0300

    Código A* com caminho

--- quebra-cabeca/AEstrela.js
+++ quebra-cabeca/AEstrela.js
@@ -1,9 +1,10 @@
 export class AEstrela {
     constructor(puzzle) {
-        this.puzzle =puzzle;
+        this.puzzle = puzzle;
         this.quebraCabeca = puzzle.getQuebraCabeca();
         this.movimentos = 0; 
         this.solucao = puzzle.getSolucao();
+        this.caminhoSolucao = [];
 
         this.heuristicaTabuleiros = new Map();
         this.solucoes = new Map();
@@ -26,33 +27,53 @@ export class AEstrela {
             
             if (this.puzzle.estaResolvido(estadoAtual.tabuleiro)) {
                 console.log("Solução encontrada em", estadoAtual.movimentos, "movimentos");
-                return;
+                this.caminhoSolucao = estadoAtual.caminho; 
+                this.imprimirCaminho(); 
+                return estadoAtual.caminho; 
             }
             
             this.puzzle.setQuebraCabeca(estadoAtual.tabuleiro);
-            const tabuleirosPossiveis =this.puzzle.getTabuleirosPossiveis(estadoAtual.tabuleiro);
+            const tabuleirosPossiveis = this.puzzle.getTabuleirosPossiveis(estadoAtual.tabuleiro);
             
             for (let tabuleiro of tabuleirosPossiveis) {
                 const tabuleiroStr = JSON.stringify(tabuleiro);
                 const novosMovimentos = estadoAtual.movimentos + 1;
                 
-                if (!this.visitado.has(tabuleiroStr) || this.visitado.get(tabuleiroStr) > novosMovimentos)  this.gerarNovoEstado(filaAberta, tabuleiro, novosMovimentos);
+                if (!this.visitado.has(tabuleiroStr) || this.visitado.get(tabuleiroStr) > novosMovimentos) {
+                    this.gerarNovoEstado(filaAberta, tabuleiro, novosMovimentos, estadoAtual.caminho);
+                }
             }
         }
         
         console.log("Sem solução encontrada");
+        return null;
     }
 
-    gerarNovoEstado(filaAberta, tabuleiro, novosMovimentos){
+    gerarNovoEstado(filaAberta, tabuleiro, novosMovimentos, caminhoAnterior) {
         const heuristica = this.calcularHeuristica(tabuleiro);
-        const custoF = novosMovimentos + heuristica;        
-        const novoEstado = { tabuleiro: tabuleiro, movimentos: novosMovimentos, custoF: custoF };        
+        const custoF = novosMovimentos + heuristica;
+        
+        const novoCaminho = [...caminhoAnterior, tabuleiro];
+        
+        const novoEstado = { 
+            tabuleiro: tabuleiro, 
+            movimentos: novosMovimentos, 
+            custoF: custoF,
+            caminho: novoCaminho
+        };
+        
         filaAberta.enqueue(novoEstado, custoF);
     }
 
-    iniciarFila(){
+    iniciarFila() {
         const filaAberta = new PriorityQueue();
-        const estadoInicial = { tabuleiro: this.puzzle.getQuebraCabeca(),movimentos: 0, custoF: this.calcularHeuristica(this.puzzle.getQuebraCabeca())};
+        const tabuleiroInicial = this.puzzle.getQuebraCabeca();
+        const estadoInicial = { 
+            tabuleiro: tabuleiroInicial,
+            movimentos: 0, 
+            custoF: this.calcularHeuristica(tabuleiroInicial),
+            caminho: [tabuleiroInicial] 
+        };
         filaAberta.enqueue(estadoInicial, estadoInicial.custoF);
         return filaAberta;
     }
@@ -66,6 +87,40 @@ export class AEstrela {
             }
         }
         return distancia;
+    }
+
+    getCaminhoSolucao() {
+        return this.caminhoSolucao;
+    }
+
+    imprimirCaminho() {
+        if (this.caminhoSolucao.length === 0) {
+            console.log("Nenhum caminho encontrado");
+            return;
+        }
+
+        console.log("\n=== CAMINHO DA SOLUÇÃO ===");
+        this.caminhoSolucao.forEach((tabuleiro, index) => {
+            console.log(`\nPasso ${index}:`);
+            this.imprimirTabuleiro(tabuleiro);
+        });
+    }
+
+    imprimirTabuleiro(tabuleiro) {
+        const tamanho = tabuleiro.length;
+        const larguraCelula = Math.max(...tabuleiro.flat()).toString().length + 1;
+        const linhaHorizontal = "+" + ("-".repeat(larguraCelula) + "+").repeat(tamanho);
+        
+        console.log(linhaHorizontal);
+        for (let i = 0; i < tamanho; i++) {
+            let linha = "|";
+            for (let j = 0; j < tamanho; j++) {
+                let valor = tabuleiro[i][j] === 0 ? " " : tabuleiro[i][j].toString();
+                linha += valor.padStart(larguraCelula, " ") + "|";
+            }
+            console.log(linha);
+            console.log(linhaHorizontal);
+        }
     }
 }
 
